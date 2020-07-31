47008fa06f36bd6d6fd52ca32d304eaef6e656ba
AntonSLaM <antonslam29@gmail.com>
2020-07-31 21:04:10 +0300

    Start

---
+++ src/main/java/com/example/courses/controllers/Main.java
@@ -0,0 +1,16 @@
+package com.example.courses.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Main {
+
+    @GetMapping("/")
+    public String home( Model model) {
+        return "index";
+    }
+
+}
